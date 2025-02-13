a3fd6f0c8a574023f3b2fdeb538259c1b0a1e78e
Andriynov <148056216+Tima126@users.noreply.github.com>
2025-02-13 11:44:30 +0300

    Update АдминистрированиеБД.sql

--- АдминистрированиеБД.sql
+++ АдминистрированиеБД.sql
@@ -1,4 +1,4 @@
--- Создание логинов на уровне сервера
+-- РЎРѕР·РґР°РЅРёРµ Р»РѕРіРёРЅРѕРІ РЅР° СѓСЂРѕРІРЅРµ СЃРµСЂРІРµСЂР°
 CREATE LOGIN AdminLogin WITH PASSWORD = 'Admin@123';
 CREATE LOGIN MedStaffLogin WITH PASSWORD = 'MedStaff@123';
 CREATE LOGIN RegistrarLogin WITH PASSWORD = 'Registrar@123';
@@ -6,66 +6,66 @@ CREATE LOGIN DoctorLogin WITH PASSWORD = 'Doctor@123';
 CREATE LOGIN PatientLogin WITH PASSWORD = 'Patient@123';
 
 
---- пользователи для БД
+--- РїРѕР»СЊР·РѕРІР°С‚РµР»Рё РґР»СЏ Р‘Р”
 CREATE USER AdminUser FOR LOGIN AdminLogin;
 CREATE USER MedStaffUser FOR LOGIN MedStaffLogin;
 CREATE USER RegistrarUser FOR LOGIN RegistrarLogin;
 CREATE USER DoctorUser FOR LOGIN DoctorLogin;
 CREATE USER PatientUser FOR LOGIN PatientLogin;
 
---- роли 
+--- СЂРѕР»Рё 
 CREATE ROLE AdminRole;
 CREATE ROLE MedStaffRole;
 CREATE ROLE RegistrarRole;
 CREATE ROLE DoctorRole;
 CREATE ROLE PatientRole;
 
--- Назначение пользователей ролям
+-- РќР°Р·РЅР°С‡РµРЅРёРµ РїРѕР»СЊР·РѕРІР°С‚РµР»РµР№ СЂРѕР»СЏРј
 ALTER ROLE AdminRole ADD MEMBER AdminUser;
 ALTER ROLE MedStaffRole ADD MEMBER MedStaffUser;
 ALTER ROLE RegistrarRole ADD MEMBER RegistrarUser;
 ALTER ROLE DoctorRole ADD MEMBER DoctorUser;
 ALTER ROLE PatientRole ADD MEMBER PatientUser;
 
--- Предоставление прав ролям
+-- РџСЂРµРґРѕСЃС‚Р°РІР»РµРЅРёРµ РїСЂР°РІ СЂРѕР»СЏРј
 
--- Права для роли Администратор (полный доступ ко всем таблицам)
-GRANT CONTROL ON DATABASE::больница TO AdminRole;
+-- РџСЂР°РІР° РґР»СЏ СЂРѕР»Рё РђРґРјРёРЅРёСЃС‚СЂР°С‚РѕСЂ (РїРѕР»РЅС‹Р№ РґРѕСЃС‚СѓРї РєРѕ РІСЃРµРј С‚Р°Р±Р»РёС†Р°Рј)
+GRANT CONTROL ON DATABASE::Р±РѕР»СЊРЅРёС†Р° TO AdminRole;
 
--- Права для роли Медицинский персонал (чтение, вставка, обновление данных)
+-- РџСЂР°РІР° РґР»СЏ СЂРѕР»Рё РњРµРґРёС†РёРЅСЃРєРёР№ РїРµСЂСЃРѕРЅР°Р» (С‡С‚РµРЅРёРµ, РІСЃС‚Р°РІРєР°, РѕР±РЅРѕРІР»РµРЅРёРµ РґР°РЅРЅС‹С…)
 GRANT SELECT, INSERT, UPDATE ON SCHEMA::dbo TO MedStaffRole;
 
--- Права для роли Регистратор (добавление и просмотр записей)
-GRANT SELECT, INSERT ON OBJECT::dbo.Записи TO RegistrarRole;
+-- РџСЂР°РІР° РґР»СЏ СЂРѕР»Рё Р РµРіРёСЃС‚СЂР°С‚РѕСЂ (РґРѕР±Р°РІР»РµРЅРёРµ Рё РїСЂРѕСЃРјРѕС‚СЂ Р·Р°РїРёСЃРµР№)
+GRANT SELECT, INSERT ON OBJECT::dbo.Р—Р°РїРёСЃРё TO RegistrarRole;
 
--- Права для роли Врач (доступ к данным своих пациентов)
-GRANT SELECT, UPDATE ON OBJECT::dbo.Пациенты TO DoctorRole;
-GRANT SELECT, UPDATE ON OBJECT::dbo.Записи TO DoctorRole;
+-- РџСЂР°РІР° РґР»СЏ СЂРѕР»Рё Р’СЂР°С‡ (РґРѕСЃС‚СѓРї Рє РґР°РЅРЅС‹Рј СЃРІРѕРёС… РїР°С†РёРµРЅС‚РѕРІ)
+GRANT SELECT, UPDATE ON OBJECT::dbo.РџР°С†РёРµРЅС‚С‹ TO DoctorRole;
+GRANT SELECT, UPDATE ON OBJECT::dbo.Р—Р°РїРёСЃРё TO DoctorRole;
 
--- Права для роли Пациент (ограниченный доступ к своим данным)
-GRANT SELECT ON OBJECT::dbo.Пациенты TO PatientRole WHERE PatientID = USER_ID();
-GRANT SELECT ON OBJECT::dbo.Записи TO PatientRole WHERE PatientID = USER_ID();
+-- РџСЂР°РІР° РґР»СЏ СЂРѕР»Рё РџР°С†РёРµРЅС‚ (РѕРіСЂР°РЅРёС‡РµРЅРЅС‹Р№ РґРѕСЃС‚СѓРї Рє СЃРІРѕРёРј РґР°РЅРЅС‹Рј)
+GRANT SELECT ON OBJECT::dbo.РџР°С†РёРµРЅС‚С‹ TO PatientRole WHERE PatientID = USER_ID();
+GRANT SELECT ON OBJECT::dbo.Р—Р°РїРёСЃРё TO PatientRole WHERE PatientID = USER_ID();
 
 
 
--- Запрет на удаление данных для Медицинского персонала
+-- Р—Р°РїСЂРµС‚ РЅР° СѓРґР°Р»РµРЅРёРµ РґР°РЅРЅС‹С… РґР»СЏ РњРµРґРёС†РёРЅСЃРєРѕРіРѕ РїРµСЂСЃРѕРЅР°Р»Р°
 DENY DELETE ON SCHEMA::dbo TO MedStaffRole;
 
--- Запрет на изменение и удаление для Регистратора
-DENY UPDATE, DELETE ON OBJECT::dbo.Записи TO RegistrarRole;
+-- Р—Р°РїСЂРµС‚ РЅР° РёР·РјРµРЅРµРЅРёРµ Рё СѓРґР°Р»РµРЅРёРµ РґР»СЏ Р РµРіРёСЃС‚СЂР°С‚РѕСЂР°
+DENY UPDATE, DELETE ON OBJECT::dbo.Р—Р°РїРёСЃРё TO RegistrarRole;
 
--- Запрет на доступ к другим таблицам для Пациента
-DENY SELECT ON OBJECT::dbo.Врачи TO PatientRole;
-DENY SELECT ON OBJECT::dbo.МедКарты TO PatientRole;
+-- Р—Р°РїСЂРµС‚ РЅР° РґРѕСЃС‚СѓРї Рє РґСЂСѓРіРёРј С‚Р°Р±Р»РёС†Р°Рј РґР»СЏ РџР°С†РёРµРЅС‚Р°
+DENY SELECT ON OBJECT::dbo.Р’СЂР°С‡Рё TO PatientRole;
+DENY SELECT ON OBJECT::dbo.РњРµРґРљР°СЂС‚С‹ TO PatientRole;
 
--- смена пароля для пользователя
+-- СЃРјРµРЅР° РїР°СЂРѕР»СЏ РґР»СЏ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ
 ALTER LOGIN AdminLogin WITH PASSWORD = 'NewAdmin@123';
 
--- отключения пользователя
+-- РѕС‚РєР»СЋС‡РµРЅРёСЏ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ
 ALTER LOGIN MedStaffLogin DISABLE;
 
---  удаления пользователя из базы данных
+--  СѓРґР°Р»РµРЅРёСЏ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ РёР· Р±Р°Р·С‹ РґР°РЅРЅС‹С…
 DROP USER RegistrarUser;
 
--- удаления логина с сервера
+-- СѓРґР°Р»РµРЅРёСЏ Р»РѕРіРёРЅР° СЃ СЃРµСЂРІРµСЂР°
 DROP LOGIN RegistrarLogin;
